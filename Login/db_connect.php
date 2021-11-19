35262d77ad268665c9595f75e06112024eaf237e
BhaveyM <76905631+BhaveyM@users.noreply.github.com>
2021-11-19 14:23:01 +0530

    edit

--- Login/db_connect.php
+++ Login/db_connect.php
@@ -1,10 +1,10 @@
 <?php
 $username="root";
 $password="";
-$dataabse="shopx6502";
+$database="shopx6502";
 $server="localhost";
 
-$conn=mysqli_connect($server,$username,$password,$dataabse);
+$conn=mysqli_connect($server,$username,$password,$database);
 if(!$conn){
     die("error".mysqli_connect_error());
 }
